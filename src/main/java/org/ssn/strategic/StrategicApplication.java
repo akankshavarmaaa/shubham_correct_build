80f078707727fe51c9b047ce2b95694bdfe73823
akankshavarma <akankshavarma48@gmail.com>
2023-01-02 02:40:13 +0530

    correct

--- src/main/java/org/ssn/strategic/StrategicApplication.java
+++ src/main/java/org/ssn/strategic/StrategicApplication.java
@@ -2,9 +2,12 @@ package org.ssn.strategic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class StrategicApplication {
+public class StrategicApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StrategicApplication.class, args);
